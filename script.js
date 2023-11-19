bb9cd422542d87d520527419b28b9ed1bac904a4
Farukh <akbfarukh@gmail.com>
2023-11-19 22:39:58 +0600

    modifided

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const apiUrl = 'https://www.themealdb.com/api/json/v2/1/latest.php'
-const row = document.querySelector('.row')
-
+const apiUrlLatest = 'https://www.themealdb.com/api/json/v2/1/latest.php'
+const apiUrlName = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+const latestMeal = document.querySelector('#latest-meal')
+const search = document.querySelector('#search')
+const latest = document.querySelector('.latest')
+const searchBlock = document.querySelector('#search-block')
+const homeBtn = document.querySelector('#home-btn')
+const searchBtn = document.querySelector('#search-btn')
+const card = document.querySelector('#card')
 const handleGetLatestMeals = () => {
-    fetch(apiUrl)
+    fetch(apiUrlLatest)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
             data.meals.forEach(meal => {
-                row.innerHTML += `
+                latestMeal.innerHTML += `
                      <div class="col-3">
                         <div class="box">
                             <img class="img-meal" src="${meal.strMealThumb}" alt="">
@@ -17,6 +22,51 @@ const handleGetLatestMeals = () => {
                 `
             })
         })
+    latest.classList.remove('hidden')
+    searchBlock.classList.add('hidden')
+    card.classList.add('hidden')
 }
+
+const handleGetMealByName = () => {
+    const searchInput = document.querySelector('#search-input')
+    let name = searchInput.value
+    fetch(`${apiUrlName}${name}`)
+        .then(response => response.json())
+        .then(data => {
+            data.meals.forEach(meal => {
+                card.innerHTML += `
+                     <div class="card-img">
+                        <h2>${meal.strMeal}</h2>
+                        <img src="" alt="">
+                    </div>
+                    <div class="card-info">
+                        <h2>Ingredients</h2>
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="box">
+                                    <img src="${meal.strMealThumb}" alt="">
+                                    <h2>${meal.strIngredient1}</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `
+            })
+        })
+}
+
+searchBtn.addEventListener('click', handleGetMealByName)
+
+search.addEventListener('click', () => {
+    latest.classList.add('hidden')
+    searchBlock.classList.remove('hidden')
+    card.classList.remove('hidden')
+})
+
+homeBtn.addEventListener('click', () => {
+    latest.classList.remove('hidden')
+    searchBlock.classList.add('hidden')
+    card.classList.add('hidden')
+})
 
 handleGetLatestMeals()
